28f5d0d1680fddd583913bd80f918dd00a32a53f
bb <bbuzz318@icloud.com>
2017-06-09 10:21:40 -0400

    Sensitivity Plotting Updates

--- Sensitivity.py
+++ Sensitivity.py
@@ -1,9 +1,10 @@
 from utils.AnalysisObjs import *
+from collections import OrderedDict
 
 class Sensitivity(res_base):
     def __init__(self, path_res):
         super(Sensitivity, self).__init__(path_res)
-        self.path_pick_dirs = self._get_all_res()
+        self.results = self._get_all_res()
 
     def totals(self, var='run'):
         """ Total Runoff for Whole Year """
@@ -16,12 +17,13 @@ class Sensitivity(res_base):
                      'inf' : 'Infiltration Rate (M/D)',
                     'evap' : 'Evaporation Volume (CM)'}
 
-        ids       = ['Default', 'U1H', 'U1L']
-        arr_all   = np.ones([len(self.slr_sh), len(ids)])
+        # ids       = ['Default', 'U1H', 'U1L']
+        arr_all   = np.ones([len(self.slr_sh), len(self.results)])
+        markers   = ['D', 's', '*'] ################### need to make len of results
 
         fig, axes = plt.subplots()
-        markers   = ['D', 's', '*']
-        for i, resdir in enumerate(self.path_pick_dirs):
+
+        for i, (ID, resdir) in enumerate(self.path_pick_dirs.items()):
             dict_var = self._load_swmm(var, path=resdir)
             y = [] # store sums and plot i times only; make legends better
             # use slr to maintain order
@@ -29,7 +31,7 @@ class Sensitivity(res_base):
                 y.append(np.nansum(dict_var[str(slr)][3696:-698, :, :]))
                 arr_all[j, i] = y[-1]
             axes.scatter(self.slr, y, marker=markers[i],
-                              label=ids[i], color=self.colors[i])
+                              label=ID, color=self.colors[i])
 
         axes.legend(loc='best', frameon=True, shadow=True, facecolor='w',
                                                                 numpoints=1)
@@ -48,17 +50,21 @@ class Sensitivity(res_base):
     def _get_all_res(self):
         """ Get Paths to all Result Directories """
         path_parent   = op.dirname(self.path)
-        path_pickdirs = [op.join(path_parent, resdir, 'Pickles') for resdir in
-                       os.listdir(path_parent) if resdir.startswith('Results_')]
-        return path_pickdirs
-
-
+        res_dict      = OrderedDict()
+        for resdir in os.listdir(path_parent):
+            if resdir.startswith('Results_'):
+                res_id       = resdir.split('_')[1]
+                path_pickdir = op.join(path_parent, resdir, 'Pickles')
+                res_dict[res_id] = path_pickdir
+        # path_pickdirs = [op.join(path_parent, resdir, 'Pickles') for resdir in
+                    #    os.listdir(path_parent) if resdir.startswith('Results_')]
+        return res_dict
 # could be a self contained object, but need slr and save stuff
 
 PATH_res       = op.join('/', 'Volumes', 'BB_4TB', 'Thesis', 'Results_05-25')
 sensitivityObj = Sensitivity(PATH_res)
-sensitivityObj.totals('inf')
-sensitivityObj.totals('evap')
-sensitivityObj.totals('run')
-savefigs(PATH_res)
-plt.show()
+# sensitivityObj.totals('inf')
+# sensitivityObj.totals('evap')
+# sensitivityObj.totals('run')
+# savefigs(PATH_res)
+# plt.show()
