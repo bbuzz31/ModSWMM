9ec684b84ff94a053c524a700ccce81318c4f3f2
bb <bbuzz318@icloud.com>
2017-11-27 12:09:38 -0500

    Refactor Indicator development and plotting

--- utils/Wetlands.py
+++ utils/Wetlands.py
@@ -1,6 +1,72 @@
 from AnalysisObjs import *
 import seaborn as sns
 
+class Wetlands(res_base):
+    def __init__(self, path_results):
+        super(Wetlands, self).__init__(path_results)
+        self.mat_dtw = self._make_dtw()
+        self.df_subs, self.mat_wetlands = self._ccap_wetlands()
+
+    def _ccap_wetlands(self):
+        """ Get CCAP wetlands df and full matrix grid """
+        df_subs = pd.read_csv(op.join(self.path_data, 'SWMM_subs.csv'))
+
+        ## convert to full grid
+        ser_landcover = df_subs.set_index('Zone').loc[:, 'Majority']
+        mat_landcover = res_base.fill_grid(ser_landcover, fill_value=-1)
+        mat_wetlands  = np.where(((mat_landcover < 13) | (mat_landcover > 18)),
+                                            np.nan, mat_landcover)
+        return (df_subs, mat_wetlands)
+
+    def _make_dtw(self, slr=0.0):
+        """ dtw, all locations all times """
+        mat_heads = np.load(op.join(self.path_picks, 'swmm_heads_grid_{}.npy'
+                                                                .format(slr)))
+        mat_z     = (np.load(op.join(self.path_data, 'Land_Z.npy'))
+                                                .reshape(self.nrows, self.ncols))
+        mat_dtw   = (mat_z - mat_heads).reshape(mat_heads.shape[0], -1)
+        return mat_dtw.T
+
+    def make_indicator(self, cutoff=-2500, show=False):
+        """ Indicator built from longest time at lowest DTW """
+        # highest values (closest to 0) = most hours with lowest DTW
+        mat_dtw_sum = (self.mat_dtw * -1).sum(axis=1)
+
+        ## get just highest cells
+        mat_highest = np.where(mat_dtw_sum <= cutoff, np.nan, mat_dtw_sum)
+
+        if show:
+            print ('Indicated cells: {}'.format(np.count_nonzero(~np.isnan(mat_highest))))
+            print ('Wetland cells: {}'.format(np.count_nonzero(~np.isnan(self.mat_wetlands))))
+
+        return (mat_dtw_sum, mat_highest)
+
+    def plot_indicators(self, cut=-2500):
+        """
+        Make histogram of cells, show their locations
+        Show cells above cutoff and wetland locations
+        """
+        mat_dtw_sum, mat_highest = self.make_indicator(cut)
+        mask = np.isnan(mat_dtw_sum)
+        bins = np.linspace(-5000, 0, 21)
+
+        fig, axes = plt.subplots(ncols=2, nrows=2)
+        axe       = axes.ravel()
+        axe[0].hist(mat_dtw_sum[~mask], bins=bins)
+        axe[1].imshow(mat_dtw_sum.reshape(74, -1), cmap=plt.cm.jet)
+        axe[2].imshow(mat_highest.reshape(74, -1), cmap=plt.cm.jet)
+        axe[3].imshow(self.mat_wetlands.reshape(74, -1), cmap=plt.cm.jet)
+
+        titles = ['Hist of summed negative dtws', 'Total annual DTW',
+                  'Locs of cells above dtw cutoff: {}'.format(cut), 'Locs of wetlands cells']
+        for i, t in enumerate(titles):
+            axe[i].set_title(t)
+
+        fig.subplots_adjust(top=0.95, hspace=0.5)
+
+        plt.show()
+
+
 def dtw_wetlands(path_data, path_result):
     """ Subset dtw df (all cells, annual average to just wetland cells """
     df      = pd.read_csv(op.join(path_data, 'SWMM_subs.csv'))
@@ -25,14 +91,6 @@ def comp_histograms(plot=True):
         print (df_wets['0.0'].describe())
         print (df_drys['0.0'].describe())
 
-def make_dtw(path_res, path_data, slr):
-    """ dtw, all locations all times? """
-    mat_heads = np.load(op.join(path_res, 'Pickles',
-                                    'swmm_heads_grid_{}.npy'.format(slr)))
-    mat_z     = np.load(op.join(path_data, 'Land_Z.npy')).reshape(74, 51)
-    mat_dtw   = (mat_z - mat_heads).reshape(mat_heads.shape[0], -1)
-    return mat_dtw.T
-
 def dtw_wetlands2(mat_dtw, path_data):
     """ Separate ALL dtw / cells / times by wetlands/nonwetland """
     ## convert to dtw for subsetting
@@ -48,37 +106,6 @@ def dtw_wetlands2(mat_dtw, path_data):
 
     print (df_wets.shape)
     print (df_drys.shape)
-
-def developing_indicator(mat_dtw, path_data, cutoff=-2500):
-    """ Indicator built from longest time at lowest DTW """
-    mat_dtw_neg = mat_dtw * -1
-    mat_dtw_sum = mat_dtw_neg.sum(axis=1)
-    mask = np.isnan(mat_dtw_sum)
-    bins = np.linspace(-5000, 0, 21)
-    # plt.hist(mat_dtw_sum[~mask], bins=bins)
-
-    ## show all
-    # plt.imshow(mat_dtw_sum.reshape(74, -1), cmap=plt.cm.jet)
-    # fig, axes = plt.subplots(ncols=2)
-    # axe       = axes.ravel()
-
-    ## show just highest cells
-    mat_highest = np.where(mat_dtw_sum <= cutoff, np.nan, mat_dtw_sum)
-    # axe[0].imshow(mat_highest.reshape(74, -1), cmap=plt.cm.jet)
-
-    ## show just wetland cells
-    df_subs = pd.read_csv(op.join(path_data, 'SWMM_subs.csv'))
-    ## convert to full grid
-    ser_landcover = df_subs.set_index('Zone').loc[:, 'Majority']
-    mat_landcover = res_base.fill_grid(ser_landcover, fill_value=-1)
-    mat_wetlands  = np.where(((mat_landcover < 13) | (mat_landcover > 18)), np.nan, mat_landcover)
-    # axe[1].imshow(mat_wetlands.reshape(74, -1), cmap=plt.cm.jet)
-    # sns.heatmap(mat_dtw)
-
-    return (mat_highest, mat_wetlands)
-    # print ('Indicated cells: {}'.format(np.count_nonzero(~np.isnan(mat_highest))))
-    # print ('Wetland cells: {}'.format(np.count_nonzero(~np.isnan(mat_wetlands))))
-    # plt.show()
 
 def testing_indicator(mat_dtw, path_data, cutoff):
     """ Compare results form 'developing indicator' to actual CCAP wetlands """
@@ -110,6 +137,9 @@ def optimize(mat_dtw, path_data):
 
 PATH_res = op.join(op.expanduser('~'), 'Google_Drive',
                     'WNC', 'Wetlands_Paper', 'Results_Default')
+Wetlands(PATH_res).plot_indicator()
+
+os.sys.exit()
 Result   = res_base(PATH_res)
 
 
